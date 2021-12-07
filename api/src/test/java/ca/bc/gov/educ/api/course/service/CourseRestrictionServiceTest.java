b7e09139fe2dcf698e95985422ca5d5dd95953e5
Jinil Sung <infstar@gmail.com>
2021-12-07 09:22:17 -0800

    Fixed the failed unit tests.
    Fixed the failed unit tests.

--- api/src/test/java/ca/bc/gov/educ/api/course/service/CourseRestrictionServiceTest.java
+++ api/src/test/java/ca/bc/gov/educ/api/course/service/CourseRestrictionServiceTest.java
@@ -12,6 +12,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.web.reactive.function.client.WebClient;
 
 import java.sql.Date;
 import java.util.Arrays;
@@ -33,6 +34,9 @@ public class CourseRestrictionServiceTest {
 
     @MockBean
     private CourseRestrictionRepository courseRestrictionRepository;
+
+    @MockBean
+    WebClient webClient;
 
     @Test
     public void testGetAllCourseRestrictionList() {
