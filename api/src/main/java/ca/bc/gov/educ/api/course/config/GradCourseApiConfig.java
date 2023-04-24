1f945bb7020697d217ececa0e6503cf3b49aaae6
alexryba <alex.rybakov@gov.bc.ca>
2023-04-24 10:55:16 -0600

    fix timezone config

--- api/src/main/java/ca/bc/gov/educ/api/course/config/GradCourseApiConfig.java
+++ api/src/main/java/ca/bc/gov/educ/api/course/config/GradCourseApiConfig.java
@@ -4,9 +4,13 @@ import ca.bc.gov.educ.api.course.model.dto.Course;
 import ca.bc.gov.educ.api.course.model.dto.StudentCourse;
 import ca.bc.gov.educ.api.course.model.entity.CourseEntity;
 import ca.bc.gov.educ.api.course.model.entity.StudentCourseEntity;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
+
+import java.util.TimeZone;
 
 @Configuration
 public class GradCourseApiConfig {
@@ -21,5 +25,18 @@ public class GradCourseApiConfig {
         modelMapper.typeMap(StudentCourseEntity.class, StudentCourse.class);
         modelMapper.typeMap(StudentCourse.class, StudentCourseEntity.class);
         return modelMapper;
+    }
+
+    @Bean
+    ObjectMapper jacksonObjectMapper(Jackson2ObjectMapperBuilder builder) {
+        return builder.createXmlMapper(false)
+                // Set timezone for JSON serialization as system timezone
+                .timeZone(TimeZone.getDefault())
+                .build();
+    }
+
+    @Bean
+    Jackson2ObjectMapperBuilder jackson2ObjectMapperBuilder() {
+        return new Jackson2ObjectMapperBuilder();
     }
 }
