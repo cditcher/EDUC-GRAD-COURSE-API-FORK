22715f571862d16286a1c4ab39ab368e7e6cf439
Kamal Mohammed <kamal.mohammed@outlook.com>
2024-01-10 10:27:59 -0700

    Added update config-map script

---
+++ tools/config/update-configmap.sh
@@ -0,0 +1,63 @@
+###########################################################
+#ENV VARS
+###########################################################
+envValue=$1
+APP_NAME=$2
+GRAD_NAMESPACE=$3
+COMMON_NAMESPACE=$4
+BUSINESS_NAMESPACE=$5
+SPLUNK_TOKEN=$6
+
+SPLUNK_URL="gww.splunk.educ.gov.bc.ca"
+FLB_CONFIG="[SERVICE]
+   Flush        1
+   Daemon       Off
+   Log_Level    info
+   HTTP_Server   On
+   HTTP_Listen   0.0.0.0
+   Parsers_File parsers.conf
+[INPUT]
+   Name   tail
+   Path   /mnt/log/*
+   Exclude_Path *.gz,*.zip
+   Parser docker
+   Mem_Buf_Limit 20MB
+[FILTER]
+   Name record_modifier
+   Match *
+   Record hostname \${HOSTNAME}
+[OUTPUT]
+   Name   stdout
+   Match  absolutely_nothing_bud
+   Log_Level    off
+[OUTPUT]
+   Name  splunk
+   Match *
+   Host  $SPLUNK_URL
+   Port  443
+   TLS         On
+   TLS.Verify  Off
+   Message_Key $APP_NAME
+   Splunk_Token $SPLUNK_TOKEN
+"
+PARSER_CONFIG="
+[PARSER]
+    Name        docker
+    Format      json
+"
+###########################################################
+#Setup for config-maps
+###########################################################
+echo Creating config map "$APP_NAME"-config-map
+oc create -n "$OPENSHIFT_NAMESPACE"-"$envValue" configmap "$APP_NAME"-config-map \
+  --from-literal=APP_LOG_LEVEL="INFO" \
+  --from-literal=ENABLE_FLYWAY="true" \
+  --from-literal=GRAD_PROGRAM_API="http://educ-grad-program-api.$GRAD_NAMESPACE-$envValue.svc.cluster.local:8080/" \
+  --from-literal=MAX_RETRY_ATTEMPTS="3" \
+  --dry-run=client -o yaml | oc apply -f -
+
+echo Creating config map "$APP_NAME"-flb-sc-config-map
+oc create -n "$OPENSHIFT_NAMESPACE"-"$envValue" configmap "$APP_NAME"-flb-sc-config-map \
+  --from-literal=fluent-bit.conf="$FLB_CONFIG" \
+  --from-literal=parsers.conf="$PARSER_CONFIG" \
+  --dry-run=client -o yaml | oc apply -f -
