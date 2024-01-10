7cc0f9a14dabbf9dd9fa1ed72e589e30941e23b7
Kamal Mohammed <kamal.mohammed@outlook.com>
2024-01-10 14:07:33 -0700

    Added update config-map script

--- tools/config/update-configmap.sh
+++ tools/config/update-configmap.sh
@@ -49,7 +49,7 @@ PARSER_CONFIG="
 #Setup for config-maps
 ###########################################################
 echo Creating config map "$APP_NAME"-config-map
-oc create -n "$OPENSHIFT_NAMESPACE"-"$envValue" configmap "$APP_NAME"-config-map \
+oc create -n "GRAD_NAMESPACE"-"$envValue" configmap "$APP_NAME"-config-map \
   --from-literal=APP_LOG_LEVEL="INFO" \
   --from-literal=ENABLE_FLYWAY="true" \
   --from-literal=GRAD_PROGRAM_API="http://educ-grad-program-api.$GRAD_NAMESPACE-$envValue.svc.cluster.local:8080/" \
@@ -57,7 +57,7 @@ oc create -n "$OPENSHIFT_NAMESPACE"-"$envValue" configmap "$APP_NAME"-config-map
   --dry-run=client -o yaml | oc apply -f -
 
 echo Creating config map "$APP_NAME"-flb-sc-config-map
-oc create -n "$OPENSHIFT_NAMESPACE"-"$envValue" configmap "$APP_NAME"-flb-sc-config-map \
+oc create -n "GRAD_NAMESPACE"-"$envValue" configmap "$APP_NAME"-flb-sc-config-map \
   --from-literal=fluent-bit.conf="$FLB_CONFIG" \
   --from-literal=parsers.conf="$PARSER_CONFIG" \
   --dry-run=client -o yaml | oc apply -f -
